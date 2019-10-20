81065ffd72400b5c60cf4f84c93a9c08c5199537
Joe Byrd <byrd1ja@cmich.edu>
2019-10-20 15:50:44 -0400

    Adding Game Master Object

---
+++ Assets/GameMasterScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMasterScript : MonoBehaviour {
+    int currentFrame;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("I am being run when the Game Master is intialized!");
+        currentFrame = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentFrame++;
+		Debug.Log("I am being logged every frame. Current Frame: " + currentFrame);
+	}
+}
