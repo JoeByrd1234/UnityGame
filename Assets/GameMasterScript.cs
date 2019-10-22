9ba4664a9909ba7d0840cffef4f71cd73d99cfd9
Joe Byrd <byrd1ja@cmich.edu>
2019-10-21 20:44:17 -0400

    Adding first scene

--- Assets/GameMasterScript.cs
+++ Assets/GameMasterScript.cs
@@ -3,17 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameMasterScript : MonoBehaviour {
-    int currentFrame;
+    
+    public GameObject squarePrefab;
 
-	// Use this for initialization
+    public int GRID_SIZE = 5;
+
+    public float SIDE_LENGTH = 1;
+
 	void Start () {
-		Debug.Log("I am being run when the Game Master is intialized!");
-        currentFrame = 0;
+		Debug.Log("Game Master init...");
+
+        GameObject[,] grid = generateRandomGrid(GRID_SIZE);
+        displayGrid(grid);
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        currentFrame++;
-		Debug.Log("I am being logged every frame. Current Frame: " + currentFrame);
+
 	}
+
+    /*
+     * Generates a random grid of variable size.
+     * */
+     GameObject[,] generateRandomGrid(int gridSize) {
+        GameObject[,] newGrid = new GameObject[gridSize, gridSize];
+
+        for (int r = 0; r < gridSize; r++) {
+            for (int c = 0; c < gridSize; c++) {
+                // Calling our method below to get a random square
+                newGrid[r,c] = generateRandomSquare();
+            }
+        }
+
+        return newGrid;
+    }
+
+    /*
+     * When we generate random grids, we need to generate random squares
+     * as well. This method shows how this can be done at runtime.
+     * */
+    GameObject generateRandomSquare() {
+        // The square we return is spawned with the "Instantiate" method
+        // It accepts a prefab (linked in the editor), a Vector3 for position, and a rotation description
+        GameObject ret = Instantiate(squarePrefab, new Vector3(), Quaternion.identity);
+        ret.GetComponent<SpriteRenderer>().color = new Color(Random.value, Random.value, Random.value);
+        return ret;
+    }
+
+    /*
+     * Displays the grid by placing all the squares in their rightful
+     * locations.
+     * */
+    void displayGrid(GameObject[,] grid) {
+        for (int r = 0; r < GRID_SIZE; r++) {
+            for (int c = 0; c < GRID_SIZE; c++) {
+                grid[r,c].transform.position = new Vector3(c * SIDE_LENGTH, -r * SIDE_LENGTH, 0);
+            }
+        }
+    }
 }
